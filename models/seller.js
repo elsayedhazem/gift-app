17d5a1285f1f7eed943591a3225e11f158eacfdf
Hazem Elsayed <hazemm.els@gmail.com>
2021-06-30 16:11:51 +0300

    Added Seller Model

---
+++ models/seller.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { capitalize } = require("./utils");
+
+const SellerSchema = Schema({
+  Name: {
+    required: true,
+    type: String,
+    set: capitalize,
+  },
+  dateJoined: { type: Number, required: true },
+  imgUrl: String,
+  representativeFirstName: {
+    type: String,
+    required: true,
+    set: capitalize,
+  },
+  representativeEmail: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  representativePassword: {
+    hash: { type: String, required: true },
+    salt: { type: Number, required: true },
+    required: true,
+    type: String,
+  },
+  sellerUUID: {
+    hash: { type: String, required: true },
+    salt: { type: Number, required: true },
+    required: true,
+    type: String,
+  },
+  stores: [{ type: Schema.Types.ObjectId, ref: "Store" }],
+});
+
+module.exports = mongoose.model("Seller", SellerSchema);
